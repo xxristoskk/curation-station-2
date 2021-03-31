ff9968048c666c77d1f52bf094a427caeb26998d
xxristoskk <katsaros.christopher@gmail.com>
2021-03-31 01:57:06 -0400

    fixing allowed host

--- bandmapV2/settings.py
+++ bandmapV2/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = os.environ['SECRET_KEY']
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['bandmapV2.herokuapp.com/']
+ALLOWED_HOSTS = ['curation-station-2.herokuapp.com']
 
 
 # Application definition
