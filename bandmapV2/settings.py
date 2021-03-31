4c302b4916b96d0ec45946331c69148a9e3280db
xxristoskk <katsaros.christopher@gmail.com>
2021-03-31 14:52:33 -0400

    changed templates dir

--- bandmapV2/settings.py
+++ bandmapV2/settings.py
@@ -58,7 +58,7 @@ ROOT_URLCONF = 'bandmapV2.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['frontpage/templates/'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
